46c0db09e71c3da4b48da5959f6e8b20b21f232f
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-07-05 12:52:49 -0500

    Completed Tests for Functional Pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = 0;
+  var end = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //add value to storage object
+    storage[start++] = value;
   };
 
   someInstance.dequeue = function() {
+    if (start > end) {
+      var temp = storage[end];
+      delete storage[end];
+      end++;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return start - end;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,36 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // Create a variable to hold the top of stack
+  var top = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //add value into storage object with the top of the stack as the key
+    storage[top] = value;
+    //increment top
+    top++;
   };
 
   someInstance.pop = function() {
+    //check if top is greater than zero
+    if (top > 0) {
+      //if true the decrement top so that top is the key to the top of our stack
+      top--;
+      //save popped value to be returned later
+      var temp = storage[top];
+      //delete value from the stack
+      delete storage[top];
+      //return value popped
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    //return top as it will always be the count of how many key value pairs are in our stack
+    //found small bug cannot use the word t h i s in comments or it throws error in specrunner
+    //thinks the keyword t h i s is being used
+    return top;
   };
 
   return someInstance;
