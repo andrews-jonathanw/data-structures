c22ccc91bd00ef9ade007078be1e47ca71a619fc
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-07-05 15:27:32 -0500

    Passing all tests for pseudoclassical pattern.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create properties for the Queue class
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
 };
 
+// Enqueue function
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.start++] = value;
+};
+
+// Dequeue function
+Queue.prototype.dequeue = function (value) {
+  if (this.start > this.end) {
+    var value = this.storage[this.end];
+    delete this.storage[this.end];
+    this.end++;
+    return value;
+  }
+};
+
+// Get size function
+Queue.prototype.size = function (value) {
+  return this.start - this.end;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create properties for the Stack class
+  this.storage = {};
+  this.top = 0;
+};
+
+// Push function
+Stack.prototype.push = function (value) {
+  this.storage[this.top++] = value;
+};
+
+// Pop function
+Stack.prototype.pop = function (value) {
+  if (this.top > 0) {
+    this.top--;
+    var value = this.storage[this.top];
+    delete this.storage[this.top];
+    return value;
+  } else {
+    return this.storage[this.top];
+  }
+};
+
+// Get size function
+Stack.prototype.size = function (value) {
+  return this.top;
 };
 
 
