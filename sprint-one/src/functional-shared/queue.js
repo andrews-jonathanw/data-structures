eb3f432a1ba10894fdcc50805d013f36baeaff26
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-07-05 13:20:50 -0500

    Passing all tests for Functional Shared Pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create an object called stack to be returned later
+  var queue = {};
+  //create storage property for the stack
+  queue.storage = {};
+  // Create top property for the stack
+  queue.start = 0;
+  queue.end = 0;
+  _.extend(queue, queueMethods);
+  // Return stack
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  // Enqueue Method
+  enqueue(value) {
+    this.storage[this.start] = value;
+    this.start++;
+  },
+
+  // Pop Method
+  dequeue() {
+    // First check the value of start is greater than end
+    if (this.start > this.end) {
+      // save the value into a variable to be return after decrement of end
+      var value = this.storage[this.end];
+      //delete from queue
+      delete this.storage[this.end];
+      this.end++;
+      return value;
+    }
+  },
+
+  // Get Size Method
+  size() {
+    return this.start - this.end;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create an object called stack to be returned later
+  var stack = {};
+  //create storage property for the stack
+  stack.storage = {};
+  // Create top property for the stack
+  stack.top = 0;
+  _.extend(stack, stackMethods);
+  // Return stack
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // Push Method
+  push(value) {
+    this.storage[this.top] = value;
+    this.top++;
+  },
+
+  // Pop Method
+  pop() {
+    // First check the value of top is greater than zero
+    if (this.top > 0) {
+      // if true decremenet first like in the functional pattern
+      this.top--;
+      var value = this.storage[this.top];
+      //delete from stack
+      delete this.storage[this.top];
+      return value;
+    } else {
+      // simply return so we dont decrement past 0
+      return this.storage[this.top];
+    }
+  },
+
+  // Get Size Method
+  size() {
+    return this.top;
+  }
+};
 
 
