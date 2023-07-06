9a4e9440d8af850a8c2e476fa6761df8252ff536
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-07-06 18:08:42 -0500

    Passing Tests for LinkedList and Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,18 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create new tree
+  var tree = new Tree(value);
+  this.children.push(tree);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, tree) {
+  // check in target in child array
+  var childs;
+  var result = false;
+  // check if tree is null
+  if (tree === undefined) {
+    childs = this.children;
+  } else {
+    childs = tree.children;
+  }
+  for (var i = 0; i < childs.length; i++) {
+    if (childs[i].value === target) {
+      return true;
+    } else {
+      if (this.contains(target, childs[i]) === true) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
+
 
 
 
